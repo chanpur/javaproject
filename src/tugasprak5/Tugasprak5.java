ed0100180e87b86772d482a8b04c5a695e4de485
dewan <dewandrajaya@gmail.com>
2018-04-29 22:43:32 +0700

    new commit

---
+++ src/tugasprak5/Tugasprak5.java
@@ -0,0 +1,12 @@
+package tugasprak5;
+
+public class Tugasprak5 {
+    
+    public static void main(String[] args){
+        inputoutput io = new inputoutput();
+        io.normal();
+        io.bonus();
+        io.bungatot();
+        io.tampilbunga();
+    }
+}
