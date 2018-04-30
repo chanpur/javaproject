030338d84d82d92e63e62f31e90347d5529762a1
jpangala <38371815+jpangala@users.noreply.github.com>
2018-04-30 14:14:57 +0700

    tambah comment

--- src/tugasprak5/Tugasprak5.java
+++ src/tugasprak5/Tugasprak5.java
@@ -3,7 +3,7 @@ package tugasprak5;
 public class Tugasprak5 {
     
     public static void main(String[] args){
-        inputoutput io = new inputoutput();
+        inputoutput io = new inputoutput();//untuk memanggil dari kelas inputoutput
         io.normal();
         io.bonus();
         io.bungatot();
